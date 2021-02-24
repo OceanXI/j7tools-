4e770b3db385a9acbf4dcc8ff7ae82959771b858
OceanXI <104946883+OceanXI@users.noreply.github.com>
2021-02-24 13:56:52 +0000

    Version 5.2.0

--- js/index.js
+++ js/index.js
@@ -19,4 +19,4 @@ function formatDay(month, day, date) {
     (date.getMonth() === month && date.getDate() > day)
   ) {
     className += " past";
-  } else if (date.getMonth() === mon
+  } else if (date.getMonth() === mont
