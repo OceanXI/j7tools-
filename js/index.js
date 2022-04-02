f174eac141dc1bc9953b4f2e5e8556722b54d414
OceanXI <104946883+OceanXI@users.noreply.github.com>
2022-04-02 10:06:09 +0000

    changed function name

--- js/index.js
+++ js/index.js
@@ -41,4 +41,4 @@ function getContent(date) {
 
 function init() {
   const date = window.mockDate || new Date();
-  const container = document.getElementById("b
+  const container = document.getElementById("bo
