c084c9b6ebc3d1667f414e4741daffac4530f014
OceanXI <104946883+OceanXI@users.noreply.github.com>
2022-10-05 20:37:10 +0000

    connect user to event loop

--- js/index.js
+++ js/index.js
@@ -46,4 +46,4 @@ function init() {
   container.innerHTML = content;
 
   const yearContainer = document.getElementById("year");
-  yearContainer.innerHTML = date.getFullYe
+  yearContainer.innerHTML = date.getFullYear
